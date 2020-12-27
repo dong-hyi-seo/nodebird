57a721eef5e8339ba39ae497a0f0e592ba55a1e3
dong-hyi-seo <seodh1217@gmail.com>
2020-12-28 01:13:16 +0900

    redux적용중

--- store/configureStore.js
+++ store/configureStore.js
@@ -6,15 +6,9 @@ import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
-//redux-thunk : 리덕스 비동기 제공
-const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
-    console.log(action);
-    return next(action);
-}
-
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
-    const middlewares = [sagaMiddleware, loggerMiddleware]; //redux-thunk
+    const middlewares = [sagaMiddleware]; //redux-saga
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
         : composeWithDevTools(applyMiddleware(...middlewares))
