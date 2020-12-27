dbea4e6c74df3305b53bb41edbe2ef275ece37d5
donghyi <seodh1217@gmail.com>
2020-12-28 00:46:13 +0900

    1. react-slick 을 사용

--- reducers/user.js
+++ reducers/user.js
@@ -4,19 +4,37 @@ export const initialState = {
     signUpData : {},
     loginData : {},
 }
-
-export const loginAction = (data) => {
+export const loginRequestAction = (data) => {
     return {
-        type : 'LOG_IN',
+        type : 'LOG_IN_REQUEST',
+        data,
+    }
+}
+export const loginSuccessAction = (data) => {
+    return {
+        type : 'LOG_IN_SUCCESS',
+        data,
+    }
+}
+export const loginFailureAction= (data) => {
+    return {
+        type : 'LOG_IN_FAILURE',
         data,
     }
 }
 
-export const logoutAction = () => {
+export const logoutRequestAction = () => {
     return {
-        type : 'LOG_OUT'
+        type : 'LOG_OUT_REQUEST'
     }
 }
+
+export const logoutSuccessAction = () => {
+    return {
+        type : 'LOG_OUT_SUCCESS'
+    }
+}
+
 
 const reducer = (state = initialState, action) => {
     switch (action.type){
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,15 +1,26 @@
 const {createWrapper} = require('next-redux-wrapper')
 import {applyMiddleware, compose, createStore} from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from '../reducers';
+import rootSaga from '../sagas';
+
+//redux-thunk : 리덕스 비동기 제공
+const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
+    console.log(action);
+    return next(action);
+}
 
 const configureStore = () => {
-    const middlewares = [];
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware, loggerMiddleware]; //redux-thunk
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
         : composeWithDevTools(applyMiddleware(...middlewares))
-    return createStore(reducer, enhancer);
+    const store = createStore(reducer, enhancer)
+    store.sagaTask = sagaMiddleware.run(rootSaga);
+    return store;
 };
 
 const wrapper = createWrapper(configureStore, {
