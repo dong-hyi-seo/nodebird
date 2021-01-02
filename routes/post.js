33499b19a4cc2fefcf0d72987a6ba7d86680d444
donghyi <seodh1217@gmail.com>
2021-01-03 02:41:23 +0900

    1. nodebird first commit

---
+++ routes/post.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/api/posts',(req, res) => {
+    res.json([
+        {id: 1, content: 'hello express'}
+    ])
+})
+
+module.exports = router;
+
